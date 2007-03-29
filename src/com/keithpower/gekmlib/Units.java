d0711785f1b7c0a98cd773dd9ea2a4056f3e4687
keith.power <keith.power@4be16884-372b-0410-a7a1-43960b1451a1>
2007-03-29 03:03:07 +0000

    Made Java 1.4 compliant

---
+++ src/com/keithpower/gekmlib/Units.java
@@ -0,0 +1,8 @@
+package com.keithpower.gekmlib;
+
+public class Units
+{
+    public static final String FRACTION = "fraction";
+    public static final String PIXELS = "pixels";
+    public static final String INSET_PIXELS = "insetPixels";
+}
